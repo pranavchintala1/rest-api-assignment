4ffe04cd01ea6ec2c2f176eca86c75ce954cb562
Pranav Chintala <pranav.chintala27@gmail.com>
2025-09-30 22:50:55 -0400

    finished api endpoints

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { randomUUID } = require('crypto');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,8 +10,63 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+users = []
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).send('Name or email missing');
+    }
+
+    const newUser = {
+        id: randomUUID(),
+        name,
+        email
+    };
+
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === req.params.id);
+
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    res.json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).send('Name or email missing');
+    }
+
+    const user = users.find(u => u.id === req.params.id);
+
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const index = users.findIndex(u => u.id === req.params.id);
+
+    if (index === -1) {
+        return res.status(404).send('User not found');
+    }
+
+    users.splice(index, 1);
+    res.status(204).send();
 });
 
 // Do not touch the code below this comment
